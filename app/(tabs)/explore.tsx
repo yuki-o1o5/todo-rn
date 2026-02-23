0d7294706bcd2e6287cc296cb10b91bbe1a1b00d
yuki-o1o5 <jkksst1712@gmail.com>
2026-02-23 13:19:54 -0800

    feat: create modal

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,7 @@
 import Button from '@/components/button';
+import CircleButton from '@/components/circle-button';
+import EmojiPicker from '@/components/emoji-picker';
+import IconButton from '@/components/icon-button';
 import ImageViewer from '@/components/image-viewer';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
@@ -10,6 +13,9 @@ export default function ExploreScreen() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
@@ -19,22 +25,57 @@ export default function ExploreScreen() {
     if (!result.canceled) {
       console.log(result);
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
   };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage || PlaceholderImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" onPress={pickImageAsync} />
-        <Button
-          label="Use this photo"
-          onPress={() => alert('You pressed a button.')}
-        />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button label="Choose a photo" onPress={pickImageAsync} />
+          <Button
+            label="Use this photo"
+            onPress={() => alert('You pressed a button.')}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <></>
+      </EmojiPicker>
     </View>
   );
 }
@@ -56,5 +97,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
